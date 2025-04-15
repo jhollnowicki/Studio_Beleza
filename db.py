211bd83dd42145840263a1aa5ffc259d430d7783
jhollnowicki <jhonatan.n.dev@gmail.com>
2025-04-15 16:54:11 -0300

    atualização para integrar banco de dados do aiven

--- db.py
+++ db.py
@@ -1,14 +1,22 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 def get_connection():
     try:
         connection = mysql.connector.connect(
-            host='localhost',  # Altere conforme necessário
-            user='root',  # Altere conforme necessário
-            password='Lnv13301381@',  # Coloque sua senha aqui
-            database='studio_beleza'  # O nome do seu banco de dados
+            host=os.getenv('DB_HOST'),
+            port=os.getenv('DB_PORT'), 
+            user=os.getenv('DB_USER'),  
+            password=os.getenv('DB_PASSWORD'),  
+            database=os.getenv('DB_NAME'), 
+            ssl_disabled=False  
         )
         return connection
     except mysql.connector.Error as err:
         print(f"Erro de conexão: {err}")
         return None
+
